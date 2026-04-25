41b3150113b69cbc66304dd095aec6e55d61e936
blekiki8-eng <blekiki8@gmail.com>
2026-04-25 21:56:18 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,1 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-import os
 
-PORT = int(os.environ.get("PORT", 8000))
-
-Handler = SimpleHTTPRequestHandler
-
-with TCPServer(("", PORT), Handler) as httpd:
-    print("Server running on port", PORT)
-    httpd.serve_forever()
